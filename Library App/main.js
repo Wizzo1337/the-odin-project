bd01c41fcdf5822cf74984ea293fe3cfbbdb1eba
Thomas Willson <thomaswilllson@yahoo.com>
2022-05-05 20:51:59 -0700

    my fist commit

---
+++ Library App/main.js
@@ -0,0 +1,11 @@
+let myLibrary = ['Where the Red Fern Grows', 'Clifford The Big Red Dog', 'Moby Dick'];
+
+class Book {
+	constructor() {
+		// the constructor...
+	}
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
