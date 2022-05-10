4af756cbf2201df0a508573aa6698b4530d5e83d
Thomas Willson <thomaswilllson@yahoo.com>
2022-05-09 20:56:37 -0700

    Added more Book Object and input forms

--- Library App/main.js
+++ Library App/main.js
@@ -1,60 +1,65 @@
 let myLibrary = [];
 
 class Book {
-	constructor() {
-		// the constructor...
+	constructor(title, author, pageNum, read) {
+		this.title = title;
+		this.author = author;
+		this.pageNum = pageNum;
+		this.read = read;
 	}
 }
+
+let fern = new Book('JRR Token', 'Where da Red Fern go', 69, true);
 
 function displayBooks() {
-	if ($input.value === '') {
-		alert('don\'t leave blank!')
-		return
+	if ($title.value === '') {
+		alert("don't leave blank!");
+		return;
 	}
-	let $flexCont = document.querySelector('.flex-container')
-	let newDiv = document.createElement('div')
+	let $flexCont = document.querySelector('.flex-container');
+	let newDiv = document.createElement('div');
 	for (let i = 0; i < myLibrary.length; i++) {
-		newDiv.textContent = myLibrary[i]
-		$flexCont.append(newDiv)
+		newDiv.textContent = myLibrary[i].title + myLibrary[i].author + myLibrary[i].pageNum;
+		$flexCont.append(newDiv);
 	}
 }
 
-let $submit = document.querySelector('#submit')
-let $input = document.querySelector('input')
-let $modalContainer = document.querySelector('.modal-container')
-let $newBook = document.querySelector('#newBook')
-let $close = document.querySelector('.close')
+let $submit = document.querySelector('#submit');
+let $title = document.querySelector('#books');
+let $author = document.querySelector('#author');
+let $pages = document.querySelector('#pages');
+let $modalContainer = document.querySelector('.modal-container');
+let $newBook = document.querySelector('#newBook');
+let $close = document.querySelector('.close');
 
-$submit.addEventListener('click', addBookToLibrary)
-$input.addEventListener('keydown', enterSubmit)
-$newBook.addEventListener('click', openModal)
-$close.addEventListener('click', closeModal)
-$modalContainer.addEventListener('click', closeModal)
+$submit.addEventListener('click', submit);
+$title.addEventListener('keydown', submit);
+$author.addEventListener('keydown', submit);
+$pages.addEventListener('keydown', submit);
+$newBook.addEventListener('click', openModal);
+$close.addEventListener('click', closeModal);
+$modalContainer.addEventListener('click', closeModal);
 
-function enterSubmit (e) {
-	if(e.key === 'Enter') {
-		if ($input.value !== '') {
-			myLibrary.push($input.value)
+function submit(e) {
+	if (e.key === 'Enter' || e.button === 0) {
+		if ($title.value !== '') {
+			myLibrary.push(new Book($title.value, $author.value, $pages.value));
+			$modalContainer.style.display = 'none';
 		}
-		displayBooks()
-		$input.value = ''
-		e.preventDefault()
+		displayBooks();
+		$title.value = '';
+		$author.value = '';
+		$pages.value = '';
+		e.preventDefault();
 	}
-}
-
-function addBookToLibrary(e) {
-	myLibrary.push($input.value)
-	displayBooks()
-	$input.value = ''
-	e.preventDefault()
 }
 
 function openModal() {
 	$modalContainer.style.display = 'block';
 }
 
-function closeModal (e) {
+function closeModal(e) {
 	if (e.target === $modalContainer || e.target === $close) {
-    $modalContainer.style.display = 'none';
-  }
+		$modalContainer.style.display = 'none';
+	}
 }
